7ef73d2183eb029af148a3dbe168db7296d49b78
Jeffeson Ribeiro de Brito <Jeffeson9898056@edu.df.senac.br>
2025-02-21 11:23:08 -0300

    criação do app pedido

---
+++ pedido/urls.py
@@ -0,0 +1,8 @@
+# urls.py do app
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('finalizar_pedido/', views.finalizar_pedido, name='finalizar_pedido'),
+   path("validaCupom/", views.validaCupom, name='validaCupom'),
+]
